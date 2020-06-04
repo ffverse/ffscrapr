f48ca1d4a35731ff0e4a196d197929416bc343ac
Tan (CaivanPC) <38083823+tanho63@users.noreply.github.com>
2020-06-03 22:37:04 -0400

    fix export

--- working.R
+++ working.R
@@ -1,3 +1,5 @@
+library(ffscrapr)
+
 conn_ssb <- ffscrapr::mfl_connect(2020,54040)
 conn_48 <- ffscrapr::mfl_connect(2020,22627)
 conn_fog <- ffscrapr::mfl_connect(2020,12608)
@@ -7,23 +9,3 @@ conn <- conn_dlf
 
 purrr::map_dfr(list(conn_ssb,conn_48,conn_fog,conn_dlf),mfl_league_summary)
 
-library(dplyr)
-library(tidyr)
-library(purrr)
-
-
-
-scoring_endpoint <- mfl_getendpoint(conn,"rules") %>%
-  purrr::pluck("content","rules","positionRules") %>%
-  tibble() %>%
-  unnest_wider(1) %>%
-  mutate(vec_depth = map_dbl(rule,vec_depth),
-         rule = case_when(vec_depth == 3 ~ map_depth(rule,2,`[[`,1),
-                          vec_depth == 4 ~ map_depth(rule,-2,`[[`,1)),
-         rule = case_when(vec_depth == 4 ~ map(rule,bind_rows),
-                          TRUE ~ rule)) %>%
-  select(-vec_depth) %>%
-  unnest_wider(rule) %>%
-  unnest(c(points,event,range)) %>%
-  separate_rows(positions,sep = "\\|") %>%
-  left_join(rule_library_mfl, by = c('event'='abbrev'))
