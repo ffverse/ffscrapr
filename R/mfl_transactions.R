3e5cfa01a46c3ee0c096fa0a4f670b74012d3aa5
Tan-DeskPC <tan@tanho.ca>
2020-07-26 21:40:04 -0400

    add latest version of transactions

--- R/mfl_transactions.R
+++ R/mfl_transactions.R
@@ -24,11 +24,14 @@ ff_transactions.mfl_conn <- function(conn,...){
     tidyr::unnest_wider(1) %>%
     dplyr::mutate_at('timestamp',~as.numeric(.x) %>% lubridate::as_datetime())
 
+  if(!"comments" %in% names(df_transactions)) df_transactions$comments <- NA_character_
+
   transaction_functions <- list(
     auction = .mfl_transactions_auction,
     free_agent = .mfl_transactions_freeagent,
     injured_reserve = .mfl_transactions_injuredreserve,
     taxi_squad = .mfl_transactions_taxisquad,
+    bbid_waiver = .mfl_transactions_bbid_waiver,
     trade = .mfl_transactions_trade
   )
 
@@ -53,11 +56,7 @@ ff_transactions.mfl_conn <- function(conn,...){
     tidyr::separate('transaction',into = c('player_id','bid_amount','comments'),sep = "\\|") %>%
     dplyr::mutate('comments' = ifelse(stringr::str_length(.data$comments)==0,
                                       NA_character_,
-                                      .data$comments)) %>%
-    dplyr::left_join(
-      dplyr::select(mfl_players(),'player_id','player_name','pos','age','team'),
-      by = 'player_id'
-    )
+                                      .data$comments))
 }
 
 ## TRADE ##
@@ -80,14 +79,15 @@ ff_transactions.mfl_conn <- function(conn,...){
     dplyr::mutate_at(c('franchise1_gave_up','franchise2_gave_up'),~stringr::str_replace(.x,",$","")) %>%
     dplyr::mutate_at(c('franchise1_gave_up','franchise2_gave_up'),~stringr::str_split(.x,","))
 
-  dplyr::select(parsed_trades,
-                .data$timestamp,
-                .data$type,
-                'franchise'=.data$franchise2,
-                'franchise2'=.data$franchise,
-                'franchise1_gave_up'=.data$franchise2_gave_up,
-                'franchise2_gave_up'=.data$franchise1_gave_up,
-                .data$comments) %>%
+  parsed_trades %>%
+    dplyr::select(
+      .data$timestamp,
+      .data$type,
+      'franchise'=.data$franchise2,
+      'franchise2'=.data$franchise,
+      'franchise1_gave_up'=.data$franchise2_gave_up,
+      'franchise2_gave_up'=.data$franchise1_gave_up,
+      .data$comments) %>%
     dplyr::bind_rows(parsed_trades) %>%
     dplyr::rename('trade_partner' = .data$franchise2,
                   'traded_for' = .data$franchise2_gave_up,
@@ -117,13 +117,14 @@ ff_transactions.mfl_conn <- function(conn,...){
     tidyr::pivot_longer(c("added","dropped"),names_to = "type_desc", values_to = "player_id") %>%
     dplyr::filter(.data$player_id!="")
 
-  dplyr::select(parsed_fa,
-                .data$timestamp,
-                .data$type,
-                .data$franchise,
-                .data$type_desc,
-                .data$player_id,
-                .data$comments) %>%
+  parsed_fa %>%
+    dplyr::select(
+      .data$timestamp,
+      .data$type,
+      .data$franchise,
+      .data$type_desc,
+      .data$player_id,
+      .data$comments) %>%
     dplyr::arrange(desc(.data$timestamp))
 
 }
@@ -147,19 +148,20 @@ ff_transactions.mfl_conn <- function(conn,...){
     tidyr::pivot_longer(c("activated","deactivated"),names_to = "type_desc", values_to = "player_id") %>%
     dplyr::filter(.data$player_id!="")
 
-  dplyr::select(parsed_ir,
-                .data$timestamp,
-                .data$type,
-                .data$franchise,
-                .data$type_desc,
-                .data$player_id,
-                .data$comments) %>%
+  parsed_ir %>%
+    dplyr::select(
+      .data$timestamp,
+      .data$type,
+      .data$franchise,
+      .data$type_desc,
+      .data$player_id,
+      .data$comments) %>%
     dplyr::arrange(desc(.data$timestamp))
 
 }
 
 ## TAXI SQUAD ##
-# Create a tibble where each injured reserve transaction is represented on one line
+# Create a tibble where each taxi squad player is represented on one line
 #' @noRd
 #' @keywords internal
 
@@ -177,27 +179,60 @@ ff_transactions.mfl_conn <- function(conn,...){
     tidyr::pivot_longer(c("promoted","demoted"),names_to = "type_desc", values_to = "player_id") %>%
     dplyr::filter(.data$player_id!="")
 
-  dplyr::select(parsed_ts,
-                .data$timestamp,
-                .data$type,
-                .data$franchise,
-                .data$type_desc,
-                .data$player_id,
-                .data$comments) %>%
+  parsed_ts %>%
+    dplyr::select(
+      .data$timestamp,
+      .data$type,
+      .data$franchise,
+      .data$type_desc,
+      .data$player_id,
+      .data$comments) %>%
     dplyr::arrange(desc(.data$timestamp))
-
 }
 
+## BBID Waiver ##
+# Create a tibble where each injured reserve transaction is represented on one line
+#' @noRd
+#' @keywords internal
+
+.mfl_transactions_bbid_waiver <- function(df_transactions){
+
+  bbid_transactions <- df_transactions %>%
+    dplyr::filter(.data$type == "BBID_WAIVER")
+
+  if(nrow(bbid_transactions)==0){return(NULL)}
+
+  parsed_bbid <- bbid_transactions %>%
+    dplyr::select('timestamp','type','franchise','transaction','comments') %>%
+    tidyr::separate("transaction",c("player_id","bbid_spent","dropped"), sep = "\\|") %>%
+    dplyr::mutate_at(c('player_id','dropped'),~stringr::str_replace(.x,",$","")) %>%
+    dplyr::mutate_at('bbid_spent',as.numeric)
+
+  parsed_bbid_drops <- parsed_bbid %>%
+    dplyr::select('timestamp','type',"dropped",'franchise','comments') %>%
+    tidyr::separate_rows("dropped",sep = ",") %>%
+    tidyr::pivot_longer("dropped",names_to = "type_desc", values_to = "player_id") %>%
+    dplyr::filter(.data$player_id != "") %>%
+    dplyr::bind_rows(parsed_bbid) %>%
+    dplyr::mutate_at("type_desc", tidyr::replace_na,"added")
+
+  parsed_bbid_drops %>%
+    dplyr::select(
+      .data$timestamp,
+      .data$type,
+      .data$franchise,
+      .data$type_desc,
+      .data$player_id,
+      .data$bbid_spent,
+      .data$comments) %>%
+    dplyr::arrange(desc(.data$timestamp))
+}
 
 ## Will need to write functions to parse each of these, then row bind them back together afterwards.
-# WAIVER
-# BBID_WAIVER
+# WAIVER - FCFS
+#
 # BBID_AUTO_PROCESS_WAIVERS
-# FREE_AGENT
 # WAIVER_REQUEST
 # BBID_WAIVER_REQUEST
-# TRADE
-# IR
-# TAXI
 # SURVIVOR_PICK
 # POOL_PICK
