3216b2b4427c92384eaa80a86869638ec33b83bc
Tan-DeskPC <tan@tanho.ca>
2020-08-02 12:42:38 -0400

    add work on draft picks

---
+++ tests/testthat/test-ff_draftpicks.R
@@ -0,0 +1,17 @@
+with_mock_api({
+  test_that("ff_draftpicks returns a tibble of draft picks",{
+
+    ssb <- mfl_connect(2020,54040)
+    ssb_picks <- ff_draftpicks(ssb)
+
+    sfb <- mfl_connect(2020,65443)
+    sfb_picks <- ff_draftpicks(sfb)
+
+    expect_s3_class(ssb_picks,"tbl_df")
+    expect_gt(nrow(ssb_picks),1)
+
+    expect_s3_class(sfb_picks,"tbl_df")
+    expect_equal(nrow(sfb_picks),0)
+
+  })
+})
