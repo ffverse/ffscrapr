406e6596e491d6a80ead4df6f5230c87c444ddb9
Tan (CaivanPC) <38083823+tanho63@users.noreply.github.com>
2020-08-13 12:27:02 -0400

    styling adjusts

--- tests/testthat/test-generics.R
+++ tests/testthat/test-generics.R
@@ -1,21 +1,21 @@
 
-test_that("Unknown platforms return errors",{
+test_that("Unknown platforms return no-method errors",{
 
   conn <- structure(list(platform = "dummy"),
             class = "dummy_platform")
 
   message <- "No method"
 
-  expect_error(ff_draft(conn),message)
-  expect_error(ff_draftpicks(conn),message)
-  expect_error(ff_league(conn),message)
-  expect_error(ff_franchises(conn),message)
-  expect_error(ff_playerscores(conn),message)
-  expect_error(ff_scoring(conn),message)
-  expect_error(ff_rosters(conn),message)
-  expect_error(ff_schedule(conn),message)
-  expect_error(ff_standings(conn),message)
-  expect_error(ff_transactions(conn),message)
-  expect_error(ff_userleagues(conn),message)
+  expect_error(ff_draft(conn), message)
+  expect_error(ff_draftpicks(conn), message)
+  expect_error(ff_league(conn), message)
+  expect_error(ff_franchises(conn), message)
+  expect_error(ff_playerscores(conn), message)
+  expect_error(ff_scoring(conn), message)
+  expect_error(ff_rosters(conn), message)
+  expect_error(ff_schedule(conn), message)
+  expect_error(ff_standings(conn), message)
+  expect_error(ff_transactions(conn), message)
+  expect_error(ff_userleagues(conn), message)
 
 })
