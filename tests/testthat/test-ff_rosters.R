4c689f6cf2db0aea406354ebc76e82ef7f7742d2
Tan-DeskPC <tan@tanho.ca>
2020-10-09 21:41:47 -0400

    add sleeper_rosters

--- tests/testthat/test-ff_rosters.R
+++ tests/testthat/test-ff_rosters.R
@@ -4,5 +4,11 @@ with_mock_api({
     ssb_rosters <- ff_rosters(ssb)
 
     expect_tibble(ssb_rosters, min.rows = 300)
+
+    jml_conn <- ff_connect(platform = "sleeper", league_id = 522458773317046272, season = 2020)
+    jml_rosters <- ff_rosters(jml_conn)
+
+    expect_tibble(jml_rosters, min.rows = 200)
+
   })
 })
