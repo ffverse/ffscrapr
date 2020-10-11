564e31d1af09afe36c49632d90d00c6dd0a69aef
Tan-DeskPC <tan@tanho.ca>
2020-10-11 09:05:54 -0400

    memoise sleeper fns

--- R/zzz.R
+++ R/zzz.R
@@ -6,16 +6,24 @@
   # Memoise specific functions
   # Timeout lengths still up for discussion
 
-  mfl_players <<- memoise::memoise(mfl_players, ~ memoise::timeout(86400))
-  mfl_allrules <<- memoise::memoise(mfl_allrules, ~ memoise::timeout(86400))
-
   dp_values <<- memoise::memoise(dp_values, ~ memoise::timeout(86400))
   dp_playerids <<- memoise::memoise(dp_playerids, ~ memoise::timeout(86400))
 
+  mfl_players <<- memoise::memoise(mfl_players, ~ memoise::timeout(86400))
+  mfl_allrules <<- memoise::memoise(mfl_allrules, ~ memoise::timeout(86400))
+
+  ff_userleagues.mfl_conn <<- memoise::memoise(ff_userleagues.mfl_conn, ~ memoise::timeout(3600))
   ff_franchises.mfl_conn <<- memoise::memoise(ff_franchises.mfl_conn, ~ memoise::timeout(86400))
   ff_scoring.mfl_conn <<- memoise::memoise(ff_scoring.mfl_conn, ~ memoise::timeout(3600))
+  ff_standings.mfl_conn <<- memoise::memoise(ff_standings.mfl_conn, ~ memoise:timeout(3600))
+  ff_schedule.mfl_conn <<- memoise::memoise(ff_schedule.mfl_conn, ~ memoise::timeout(3600))
 
+  sleeper_players <<- memoise::memoise(sleeper_players, ~ memoise::timeout(86400))
+
+  ff_userleagues.sleeper_conn <<- memoise::memoise(ff_userleagues.sleeper_conn, ~memoise::timeout(3600))
+  ff_franchises.sleeper_conn <<- memoise::memoise(ff_franchises.sleeper_conn, ~memoise::timeout(86400))
   ff_schedule.sleeper_conn <<- memoise::memoise(ff_schedule.sleeper_conn, ~ memoise::timeout(3600))
+  ff_standings.sleeper_conn <<- memoise::memoise(ff_standings.sleeper_conn, ~memoise::timeout(3600))
 
   env <- rlang::env(
     user_agent = glue::glue(
