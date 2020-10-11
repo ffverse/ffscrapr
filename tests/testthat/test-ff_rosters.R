77743ce8ecbc9a132234d8e2695b2c74d518cc8d
Tan-DeskPC <tan@tanho.ca>
2020-10-10 21:00:14 -0400

    add tests

--- tests/testthat/test-ff_rosters.R
+++ tests/testthat/test-ff_rosters.R
@@ -2,12 +2,10 @@ with_mock_api({
   test_that("ff_rosters returns a tibble", {
     ssb <- mfl_connect(2020, 54040)
     ssb_rosters <- ff_rosters(ssb)
-
     expect_tibble(ssb_rosters, min.rows = 300)
 
-    jml_conn <- ff_connect(platform = "sleeper", league_id = 522458773317046272, season = 2020)
+    jml_conn <- sleeper_connect(league_id = 522458773317046272, season = 2020)
     jml_rosters <- ff_rosters(jml_conn)
-
     expect_tibble(jml_rosters, min.rows = 200)
   })
 })
