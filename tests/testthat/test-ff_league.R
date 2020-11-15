b37ae6c819ff5bce762194dc9f35e518211a030f
Tan-DeskPC <tan@tanho.ca>
2020-11-14 20:32:10 -0500

    add work on sleeper-league

--- tests/testthat/test-ff_league.R
+++ tests/testthat/test-ff_league.R
@@ -5,7 +5,7 @@ with_mock_api({
 
     expect_tibble(dlf_league, any.missing = FALSE, min.rows = 1)
 
-    sleeper_conn <- ff_connect("sleeper", 527362181635997696, season = 2020)
-    expect_error(ff_league(sleeper_conn))
+    jml_conn <- ff_connect(platform = "sleeper", league_id = 522458773317046272, season = 2020)
+    x <- ff_league(jml_conn)
   })
 })
