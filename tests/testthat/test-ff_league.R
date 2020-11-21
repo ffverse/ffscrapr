20d27a22142b992898484b50a6610ce1f3ce59d7
Tan-Server <tan@tanho.ca>
2020-11-21 13:56:52 -0500

    add tests

--- tests/testthat/test-ff_league.R
+++ tests/testthat/test-ff_league.R
@@ -8,5 +8,9 @@ with_mock_api({
     jml_conn <- ff_connect(platform = "sleeper", league_id = 522458773317046272, season = 2020)
     jml_league <- ff_league(jml_conn)
     expect_tibble(jml_league, any.missing = FALSE, min.rows = 1)
+
+    got_conn <- fleaflicker_connect(2020,206154)
+    got_league <- ff_league(got_conn)
+
   })
 })
