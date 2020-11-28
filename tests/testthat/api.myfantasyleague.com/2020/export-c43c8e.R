c006979ec5b9774944fbbe963abe4f383247f920
Tan Ho <38083823+tanho63@users.noreply.github.com>
2020-11-28 16:56:18 -0500

    Add vignette capturing for robustness (#150)
    * cache vignettes
    
    * move vignette cache into testthat
    
    * 1.2.0.1 - add dev version flag

---
+++ tests/testthat/api.myfantasyleague.com/2020/export-c43c8e.R
@@ -0,0 +1,20 @@
+structure(list(url = "https://api.myfantasyleague.com/2020/export?TYPE=leagueSearch&SEARCH=sfbx%20conference&JSON=1", 
+    status_code = 200L, headers = structure(list(date = "Sat, 28 Nov 2020 21:17:29 GMT", 
+        server = "Apache/2.4.18 (Unix) mod_apreq2-20090110/2.8.0 mod_perl/2.0.9 Perl/v5.10.1", 
+        vary = "Accept-Encoding", `content-encoding` = "gzip", 
+        `content-length` = "316", `content-type` = "application/json; charset=utf-8", 
+        targethost = "www70"), class = c("insensitive", "list"
+    )), all_headers = list(list(status = 200L, version = "HTTP/1.1", 
+        headers = structure(list(date = "Sat, 28 Nov 2020 21:17:29 GMT", 
+            server = "Apache/2.4.18 (Unix) mod_apreq2-20090110/2.8.0 mod_perl/2.0.9 Perl/v5.10.1", 
+            vary = "Accept-Encoding", `content-encoding` = "gzip", 
+            `content-length` = "316", `content-type` = "application/json; charset=utf-8", 
+            targethost = "www70"), class = c("insensitive", "list"
+        )))), cookies = structure(list(domain = logical(0), flag = logical(0), 
+        path = logical(0), secure = logical(0), expiration = structure(numeric(0), class = c("POSIXct", 
+        "POSIXt")), name = logical(0), value = logical(0)), row.names = integer(0), class = "data.frame"), 
+    content = charToRaw("{\"version\":\"1.0\",\"leagues\":{\"league\":[{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/13411\",\"name\":\"#SFBX Conference 12\",\"id\":\"13411\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/16428\",\"name\":\"#SFBX Conference 6\",\"id\":\"16428\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/17910\",\"name\":\"#SFBX Conference 11\",\"id\":\"17910\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/26453\",\"name\":\"#SFBX Conference 8\",\"id\":\"26453\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/27495\",\"name\":\"#SFBX Conference 2\",\"id\":\"27495\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/31492\",\"name\":\"#SFBX Conference 14\",\"id\":\"31492\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/31671\",\"name\":\"#SFBX Conference 15\",\"id\":\"31671\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/35779\",\"name\":\"#SFBX Conference 13\",\"id\":\"35779\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/37784\",\"name\":\"#SFBX Conference 1\",\"id\":\"37784\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/39990\",\"name\":\"#SFBX Conference 9\",\"id\":\"39990\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/42222\",\"name\":\"#SFBX Conference 7\",\"id\":\"42222\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/43130\",\"name\":\"#SFBX Conference 3\",\"id\":\"43130\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/60168\",\"name\":\"#SFBX Conference 10\",\"id\":\"60168\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/65443\",\"name\":\"#SFBX Conference 5\",\"id\":\"65443\"},{\"homeURL\":\"http://www73.myfantasyleague.com/2020/home/69624\",\"name\":\"#SFBX Conference 4\",\"id\":\"69624\"}]},\"encoding\":\"utf-8\"}"), 
+    date = structure(1606598249, class = c("POSIXct", "POSIXt"
+    ), tzone = "GMT"), times = c(redirect = 0, namelookup = 0.041419, 
+    connect = 0.074933, pretransfer = 0.16718, starttransfer = 0.320741, 
+    total = 0.321132)), class = "response")
