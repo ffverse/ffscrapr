778c5080547eb8d990a378e7c68b34338a3a3135
Tan (CaivanPC) <38083823+tanho63@users.noreply.github.com>
2021-01-13 14:24:15 -0500

    add request shortener for espn

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -6,3 +6,9 @@ suppressPackageStartupMessages({
 if (Sys.getenv("MOCK_BYPASS") == "true") {
     with_mock_api <- force
 }
+#
+httptest::set_requester(
+  function(request){
+      httptest::gsub_request(request,"https\\://fantasy.espn.com/apis/v3/games/ffl/","espn/")
+  }
+)
