6bd4ee770e4e8be7fa9af4a283f71933326dc7ae
Tan-DeskPC <tan@tanho.ca>
2021-02-01 09:22:03 -0500

    rename tests

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -9,12 +9,20 @@ if (Sys.getenv("MOCK_BYPASS") == "true") {
 
 httptest::set_requester(
   function(request){
-      httptest::gsub_request(request,"https\\://fantasy.espn.com/apis/v3/games/ffl/","espn/")
+    httptest::gsub_request(request,"https\\://fantasy.espn.com/apis/v3/games/ffl/","espn/") %>%
+      httptest::gsub_request("https\\://api.myfantasyleague.com/","mfl/") %>%
+      httptest::gsub_request("https\\://api.sleeper.app/","sleeper/") %>%
+      httptest::gsub_request("https\\://www.fleaflicker.com/","flea/") %>%
+      httptest::gsub_request("https\\://github.com/DynastyProcess/data/raw/master/files/","ghdynastyprocess/")
   }
 )
 
 httptest::set_redactor(
   function(response){
-    httptest::gsub_response(response,"https\\://fantasy.espn.com/apis/v3/games/ffl/","espn/")
+    httptest::gsub_response(response,"https\\://fantasy.espn.com/apis/v3/games/ffl/","espn/") %>%
+      httptest::gsub_response("https\\://api.myfantasyleague.com/","mfl/") %>%
+      httptest::gsub_response("https\\://api.sleeper.app/","sleeper/") %>%
+      httptest::gsub_response("https\\://www.fleaflicker.com/","flea/") %>%
+      httptest::gsub_response("https\\://github.com/DynastyProcess/data/raw/master/files/","ghdynastyprocess/")
   }
 )
