4eeaf33caf5b8b6bd770806564723886c0668ea3
Tan-DeskPC <tan@tanho.ca>
2021-02-07 16:26:52 -0500

    ci skip

--- tests/testthat/test-dp_import.R
+++ tests/testthat/test-dp_import.R
@@ -12,9 +12,7 @@ with_mock_api({
 
 test_that("dp_cleannames removes periods, apostrophes, and suffixes", {
   player_names <- c("A.J. Green", "Odell Beckham Jr.", "Le'Veon Bell Sr.")
-
   cleaned_names <- dp_cleannames(player_names)
-
   lowercase_clean <- dp_cleannames(player_names, lowercase = TRUE)
 
   expect_equal(cleaned_names, c("AJ Green", "Odell Beckham", "LeVeon Bell"))
