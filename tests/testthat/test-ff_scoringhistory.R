076241a1890d7743a601e59f679e90f2f4571bae
Joe Sydlowski <syd235@gmail.com>
2021-04-03 11:51:12 -0500

    first addition for fleaflciker scoringhistory

--- tests/testthat/test-ff_scoringhistory.R
+++ tests/testthat/test-ff_scoringhistory.R
@@ -7,11 +7,11 @@ with_mock_api({
 
     # jml_conn <- ff_connect(platform = "sleeper", league_id = "522458773317046272", season = 2020)
     # expect_warning(ff_scoringhistory(jml_conn))
-    #
-    # joe_conn <- fleaflicker_connect(2020, 312861)
-    # joe_playerscores <- ff_scoringhistory(joe_conn, page_limit = 2)
-    # expect_tibble(joe_playerscores, min.rows = 50)
-    #
+
+    joe_conn <- fleaflicker_connect(2020, 312861)
+    joe_playerscores <- ff_scoringhistory(joe_conn, 2020)
+    expect_tibble(joe_playerscores, min.rows = 3000)
+
     # tony_conn <- espn_connect(season = 2020, league_id = 899513)
     # tony_playerscores <- ff_scoringhistory(tony_conn, limit = 5)
     # expect_tibble(tony_playerscores, min.rows = 5)
