2f05e6f84fa8441caae9a44a3aa9c155e646a813
Tan-DeskPC <tan@tanho.ca>
2021-05-26 22:37:51 -0400

    update test cache to dev version

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -15,13 +15,13 @@ skip <- FALSE
 if (download_mock) {
   tryCatch(
     expr = {
-      download.file("https://github.com/dynastyprocess/ffscrapr-tests/archive/1.4.2.zip", "f.zip")
+      download.file("https://github.com/dynastyprocess/ffscrapr-tests/archive/main.zip", "f.zip")
       unzip("f.zip", exdir = ".")
 
-      httptest::.mockPaths(new = "ffscrapr-tests-1.4.2")
+      httptest::.mockPaths(new = "ffscrapr-tests-main")
 
       withr::defer(
-        unlink(c("ffscrapr-tests-1.4.2", "f.zip"), recursive = TRUE, force = TRUE),
+        unlink(c("ffscrapr-tests-main", "f.zip"), recursive = TRUE, force = TRUE),
         testthat::teardown_env()
       )
     },
