d74fc85ffcf1799e10e5a44a9288788558b78af5
Tan-DeskPC <tan@tanho.ca>
2021-10-05 13:33:36 -0400

    cleanup revdep folder

--- revdep/library/ffscrapr/new/stringfish/doc/vignette.R
+++
@@ -1,7 +0,0 @@
-## ----eval=FALSE---------------------------------------------------------------
-#  install.packages("stringfish", type="source", configure.args="--with-simd=AVX2")
-
-## ----eval=FALSE---------------------------------------------------------------
-#  sf_alternate_case("hello world")
-#  [1] "hElLo wOrLd"
-
