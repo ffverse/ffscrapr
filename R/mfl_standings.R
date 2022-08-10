53fede3247e512fc18734e8b27650069a8f7beb7
Tan-DeskPC <tan@tanho.ca>
2022-08-10 11:58:23 -0400

    ff_standings.mfl updates for api changes, resolves #366

--- R/mfl_standings.R
+++ R/mfl_standings.R
@@ -17,25 +17,17 @@
 #'
 #' @export
 ff_standings.mfl_conn <- function(conn, ...) {
-  standings_endpoint <- mfl_getendpoint(conn, "leagueStandings") %>%
+  standings_endpoint <- mfl_getendpoint(conn, "leagueStandings", ALL=1) %>%
     purrr::pluck("content", "leagueStandings", "franchise") %>%
     tibble::tibble() %>%
     tidyr::unnest_wider(1) %>%
     dplyr::left_join(
-      dplyr::select(ff_franchises(conn), "franchise_id", "franchise_name"),
+      ff_franchises(conn) %>% dplyr::select("franchise_id", "franchise_name"),
       by = c("id" = "franchise_id")
     ) %>%
-    dplyr::mutate_at(dplyr::vars(-.data$id, -.data$franchise_name, -dplyr::contains("streak")), as.numeric) %>%
-    dplyr::mutate(
-      "h2h_winpct" = purrr::pmap_dbl(
-        list(.data$h2hw, .data$h2hl, .data$h2ht),
-        ~ ..1 / sum(...)
-      ),
-      "allplay_winpct" = purrr::pmap_dbl(
-        list(.data$all_play_w, .data$all_play_l, .data$all_play_t),
-        ~ ..1 / sum(...)
-      )
-    ) %>%
+    dplyr::mutate_all(~gsub(pattern = "\\$",replacement = "",x = .x)) %>%
+    dplyr::mutate_at(dplyr::vars(-.data$id, -.data$franchise_name, -dplyr::matches("streak|strk|wlt")), as.numeric) %>%
+    dplyr::mutate_at(dplyr::vars(dplyr::matches("wlt")),~gsub(x = .x, pattern = "&#8209;", replacement = "-",fixed = TRUE)) %>%
     dplyr::mutate_if(is.numeric, round, 3) %>%
     dplyr::select(dplyr::any_of(c(
       "franchise_id" = "id",
@@ -43,24 +35,31 @@ ff_standings.mfl_conn <- function(conn, ...) {
       "h2h_wins" = "h2hw",
       "h2h_losses" = "h2hl",
       "h2h_ties" = "h2ht",
-      "h2h_winpct",
+      "h2h_winpct" = "h2hpct",
+      "h2h_wlt" = "h2hwlt",
       "allplay_wins" = "all_play_w",
       "allplay_losses" = "all_play_l",
       "allplay_ties" = "all_play_t",
-      "allplay_winpct",
+      "allplay_winpct" = "all_play_pct",
+      "allplay_wlt" = "all_play_wlt",
       "points_for" = "pf",
       "points_against" = "pa",
+      "avg_points_for" = "avgpf",
+      "avg_points_against" = "avgpa",
       "max_points_against" = "maxpa",
       "min_points_against" = "minpa",
       "potential_points" = "pp",
       "victory_points" = "vp",
       "offensive_points" = "op",
       "defensive_points" = "dp",
-      # "streak_type",
-      # "streak_len",
+      "streak" = "strk",
+      "streak_type",
+      "streak_len",
       "power_rank" = "pwr",
       "power_rank_alt" = "altpwr",
-      "accounting_balance" = "acct"
+      "accounting_balance" = "acct",
+      "faab_balance" = "bbidbalance",
+      "salary"
     )))
 
   return(standings_endpoint)
