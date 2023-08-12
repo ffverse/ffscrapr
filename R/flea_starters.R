626f8aa6ec53d72db55e8793bf66e765109739e2
Randal Morris <morrrand@gmail.com>
2023-08-11 21:34:57 -0500

    Add expanded function of starters

--- R/flea_starters.R
+++ R/flea_starters.R
@@ -68,3 +68,84 @@ ff_starters.flea_conn <- function(conn, week = 1:17, ...) {
 
   return(x)
 }
+
+####  Flea ff_starters Detail ####
+
+#' Get detail of starters and bench
+#'
+#' @param conn the list object created by `ff_connect()`
+#' @param week a numeric or numeric vector
+#' @param ... other arguments (currently unused)
+#'
+#' @describeIn ff_starters Fleaflicker Detail: returns a detailed version of flea_starters with injuries and no filters
+#'
+#' @examples
+#' \donttest{
+#' try({ # try only shown here because sometimes CRAN checks are weird
+#'   conn <- fleaflicker_connect(season = 2020, league_id = 206154)
+#'   ff_starters_detail.flea_conn(conn)
+#' }) # end try
+#' }
+ff_starters_detail.flea_conn <- function(conn, week = 1:17, ...) {
+  starters <- ff_schedule(conn, week) %>%
+    dplyr::filter(!is.na(.data$result)) %>%
+    dplyr::distinct(.data$week, .data$game_id) %>%
+    dplyr::mutate(starters = purrr::map2(.data$week, .data$game_id, .flea_starters_detail, conn)) %>%
+    tidyr::unnest("starters") %>%
+    dplyr::arrange(.data$week, .data$franchise_id)
+}
+
+.flea_starters_detail <- function(week, game_id, conn) {
+  cols <- c(injurytypeAbbreviaition = NA, injurytypeFull = NA, injurydescription = NA, injuryseverity = NA)
+  x <- fleaflicker_getendpoint("FetchLeagueBoxscore",
+                               sport = "NFL",
+                               scoring_period = week,
+                               fantasy_game_id = game_id,
+                               league_id = conn$league_id
+  ) |>
+    purrr::pluck("content", "lineups") |>
+    list() |>
+    tibble::tibble() |>
+    tidyr::unnest_longer(1) |>
+    tidyr::unnest_wider(1) |>
+    tidyr::unnest_longer("slots") |>
+    tidyr::unnest_wider("slots") |>
+    dplyr::mutate(
+      position = purrr::map_chr(.data$position, purrr::pluck, "label"),
+      positionColor = NULL
+    ) |>
+    tidyr::pivot_longer(c("home", "away"), names_to = "franchise", values_to = "player") |>
+    tidyr::hoist("player", "proPlayer", "owner", "points" = "viewingActualPoints") |>
+    tidyr::hoist("proPlayer",
+                 "player_id" = "id",
+                 "player_name" = "nameFull",
+                 "pos" = "position",
+                 "team" = "proTeamAbbreviation",
+                 "injury" = "injury"
+    ) |>
+    # dplyr::filter(!is.na(.data$player_id)) |>
+    tidyr::unnest_wider("injury", names_sep = "") |>
+    tidyr::hoist("owner", "franchise_id" = "id", "franchise_name" = "name") |>
+    tidyr::hoist("points", "player_score" = "value") |>
+    dplyr::mutate(group = dplyr::case_when(is.na(group) == TRUE ~ "BENCH", .default = group))
+  x = tibble::add_column(x, !!!cols[setdiff(names(cols), names(x))]) |>
+    dplyr::select(dplyr::any_of(c(
+      "group",
+      "franchise",
+      "franchise_id",
+      "franchise_name",
+      "starter_status" = "position",
+      "player_id",
+      "player_name",
+      "pos",
+      "team",
+      "injurytypeAbbreviaition",
+      "injurytypeFull",
+      "injurydescription",
+      "injuryseverity",
+      "player_score"
+    )))
+
+
+  return(x)
+}
