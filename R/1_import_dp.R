96f56262c78f5571bb47adad9c6d5b6c675831b1
Tan Ho <tan@tanho.ca>
2023-08-14 00:07:43 -0400

    deprecate clean_names

--- R/1_import_dp.R
+++ R/1_import_dp.R
@@ -74,44 +74,6 @@ dp_playerids <- function() {
   return(content)
 }
 
-#' Clean Player Names
-#'
-#' @examples
-#' \donttest{
-#'
-#' dp_cleannames(c("A.J. Green", "Odell Beckham Jr.", "Le'Veon Bell Sr."))
-#'
-#' dp_cleannames(c("Trubisky, Mitch", "Atwell, Chatarius", "Elliott, Zeke", "Elijah Moore"),
-#'   convert_lastfirst = TRUE,
-#'   use_name_database = TRUE
-#' )
-#' }
-#'
-#' @seealso `nflreadr::clean_player_names()`
-#'
-#' @return a character vector of cleaned names
-#'
-#' @export
-dp_clean_names <- function(player_name, lowercase = FALSE, convert_lastfirst = TRUE, use_name_database = TRUE) {
-
-  lifecycle::deprecate_soft(
-    when = "1.4.8",
-    "ffscrapr::dp_clean_names",
-    "nflreadr::clean_player_names"
-  )
-
-  nflreadr::clean_player_names(
-    player_name,
-    lowercase = lowercase,
-    convert_lastfirst = convert_lastfirst,
-    use_name_database = use_name_database
-  )
-}
-
-#' @export
-#' @rdname dp_clean_names
-dp_cleannames <- dp_clean_names
-
 #' Remove HTML from string
 #'
 #' Applies some regex to clean html tags from strings. This is useful for platforms such as MFL that interpret HTML in their franchise name fields.
--- R/z_deprecated.R
+++ R/z_deprecated.R
@@ -24,3 +24,41 @@ nflfastr_rosters <- function(seasons) {
     with = "nflreadr::load_rosters()"
   )
 }
+
+#' Clean Player Names
+#'
+#' @examples
+#' \donttest{
+#'
+#' dp_cleannames(c("A.J. Green", "Odell Beckham Jr.", "Le'Veon Bell Sr."))
+#'
+#' dp_cleannames(c("Trubisky, Mitch", "Atwell, Chatarius", "Elliott, Zeke", "Elijah Moore"),
+#'   convert_lastfirst = TRUE,
+#'   use_name_database = TRUE
+#' )
+#' }
+#'
+#' @seealso `nflreadr::clean_player_names()`
+#'
+#' @return a character vector of cleaned names
+#'
+#' @export
+dp_clean_names <- function(player_name, lowercase = FALSE, convert_lastfirst = TRUE, use_name_database = TRUE) {
+
+  lifecycle::deprecate_soft(
+    when = "1.4.8",
+    "ffscrapr::dp_clean_names()",
+    "nflreadr::clean_player_names()"
+  )
+
+  nflreadr::clean_player_names(
+    player_name,
+    lowercase = lowercase,
+    convert_lastfirst = convert_lastfirst,
+    use_name_database = use_name_database
+  )
+}
+
+#' @export
+#' @rdname dp_clean_names
+dp_cleannames <- dp_clean_names
