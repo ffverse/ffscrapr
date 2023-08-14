c71d522a22e4eab8792f7643d0f9d5fbdba33d25
Tan Ho <tan@tanho.ca>
2023-08-13 21:07:15 -0400

    use usethis pkg imports

--- R/ffscrapr-package.R
+++ R/ffscrapr-package.R
@@ -4,5 +4,17 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom data.table :=
+#' @importFrom data.table .BY
+#' @importFrom data.table .EACHI
+#' @importFrom data.table .GRP
+#' @importFrom data.table .I
+#' @importFrom data.table .N
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .SD
+#' @importFrom data.table data.table
+#' @importFrom rlang .data `%||%` .env
+#' @importFrom utils str
+#' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
