c4cb6339c1ce4515378ad71eb3bfcbceddc2f635
Tan Ho <38083823+tanho63@users.noreply.github.com>
2024-05-29 14:23:09 -0400

    update httptest requestor

--- inst/httptest/request.R
+++ inst/httptest/request.R
@@ -1,6 +1,6 @@
 httptest::set_requester(
   function(request){
-    httptest::gsub_request(request,"https\\://fantasy.espn.com/apis/v3/games/ffl/","espn/") %>%
+    httptest::gsub_request(request,"https\\://.*fantasy.espn.com/apis/v3/games/ffl/","espn/") %>%
       httptest::gsub_request("https\\://api.myfantasyleague.com/","mfl/") %>%
       httptest::gsub_request("https\\://api.sleeper.app/","sleeper/") %>%
       httptest::gsub_request("https\\://www.fleaflicker.com/","flea/") %>%
